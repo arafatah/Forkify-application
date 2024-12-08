7c5bed836dc81ddf3e1190aa8759ca8a877410e7
arafatah <arafathowlader890@gmail.com>
2024-12-08 10:51:20 +0600

    start

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -1,19 +1,14 @@
 const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
-
-// Starting of project
-/* 
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+}; // https://forkify-api.herokuapp.com/v2
+ ///////////////////////////////////////
+ // Starting of project
+ /* 
 1. User Story: Description of the application's functionality from the user's perspective
 > Common formate: As a [type of user], I want [an action] so that [a benefit]. 
 
@@ -23,4 +18,6 @@ Like:
 3. As a user, I want to bookmark recipes, so that I can review them later. 
 4. As a user, I want to be able to create my own recipes, so that i have them all organized in the same app. 
 5. As a user, I want to be able to see my bookmarks and own recipes when I leave the app and come back later, so that I can close the app safely after I'm done.
-*/
+*/ 
+
+//# sourceMappingURL=index.62406edb.js.map

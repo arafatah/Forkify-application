88f54e21d0038b865a343f743c89d94a0b2ef9ad
arafatah <arafathowlader890@gmail.com>
2024-12-09 10:48:14 +0600

    test

--- src/js/controller.js
+++ src/js/controller.js
@@ -24,3 +24,74 @@ Like:
 4. As a user, I want to be able to create my own recipes, so that i have them all organized in the same app. 
 5. As a user, I want to be able to see my bookmarks and own recipes when I leave the app and come back later, so that I can close the app safely after I'm done.
 */
+
+// const getJSON = async function (url) {
+//   try {
+//     const res = await Promise.race([fetch(url), timeout(10)]);
+//     const data = await res.json();
+//     if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+//     return data;
+//   } catch (err) {
+//     throw err;
+//   }
+// };
+
+// const showRecipe = async function () {
+//   try {
+//     const data = await getJSON(
+//       `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+//     );
+//     console.log(data);
+//   } catch (err) {
+//     console.error(`${err} 💥💥💥`);
+//   }
+// };
+
+// showRecipe();
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e866f`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+
+    console.log(res, data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
+
+const showRecipe2 = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+    const data = await res.json();
+
+    if(!res.ok) throw new Error('Data was missing somehow, Req the server again.')
+    console.log(res, data);
+
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe2();
